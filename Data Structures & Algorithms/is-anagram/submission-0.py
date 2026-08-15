a1bcfc78f3c5f255ae9b2ce63bf7eb2c68b7b50b
patpawar242 <prathameshpawar2424@gmail.com>
2026-08-15 21:54:50 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_sort="".join(sorted(s))
+        t_sort="".join(sorted(t))
+        if s_sort==t_sort:
+            return True 
+        else :
+            return False
